a10af92414f3ea62214d13d691dc0ea3cb2567b9
gcamargogy <camargogdy@hotmail.com>
2024-11-01 17:33:29 -0400

    task01

---
+++ usuario.php
@@ -0,0 +1,51 @@
+<?php
+    Class Usuario
+    {
+        private $pdo;
+
+        public $msgErro = "";
+
+        public function conectar($nome, $host, $usuario, $senha)
+        {
+            global $pdo;
+
+            try
+            {
+                $pdo = new PDO("mysql:dbname=".$nome, $usuario, $senha);
+            }
+            catch (PDOException $erro)
+            {
+                $msgErro = $erro->getMessage();
+            }
+        }
+
+        public function cadastrar($nome, $telefone, $email, $senha)
+        {
+            global $pdo;
+            
+            // verificar se o email ja está cadastrado
+            $sql = $pdo->prepare("SELECT id_usuario FROM usuario WHERE email = :m");
+            $sql->bindValue(":m", $email); //:m significa que colocamos um apelido na variável email do PHP
+            $sql->execute();
+
+            // verificar se existe email cadastrado
+            if($sql->rowCount() > 0)
+            {
+                return false;
+            }
+            else
+            {
+                // cadastrar usuário
+                $sql = $pdo->prepare("INSERT INTO usuario (nome, telefone, email, senha) VALUES (:n, :t, :e, :s)");
+                $sql->bindValue(":n", $nome);
+                $sql->bindValue(":t", $telefone);
+                $sql->bindValue(":e", $email);
+                $sql->bindValue(":s", $senha);
+                $sql->execute();
+                return true;
+            }
+        }
+    }
+
+
+?>
